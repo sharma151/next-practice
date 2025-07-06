8533c1c5fd2f6df14ee84aa8c63a693fc92e4374
sharma151 <sharmasaurav150503@gmail.com>
2025-07-07 00:03:10 +0545

    navigation bar added in layout

---
+++ src/app/(user)/Components/navigationbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+const Navbar: React.FC = () => {
+  return (
+    <>
+      <nav className="bg-gray-800 p-4">
+        <div className=" flex justify-between ">
+          <div className="px-2 border  border-white items-center rounded-md pb-1">
+            {" "}
+            <Link href="/" passHref>
+              <p className="text-gray-300 hover:text-white">LOGO</p>
+            </Link>
+          </div>
+          <div className=" flex  items-center space-x-4">
+            <Link href="/" passHref>
+              <p className="text-gray-300 hover:text-white">Home</p>
+            </Link>
+            <Link href="/teams" passHref>
+              <p className="text-gray-300 hover:text-white">Teams</p>
+            </Link>
+            <Link href="/about" passHref>
+              <p className="text-gray-300 hover:text-white">About</p>
+            </Link>
+            <Link href="/contact" passHref>
+              <p className="text-gray-300 hover:text-white">Contact</p>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Navbar;
